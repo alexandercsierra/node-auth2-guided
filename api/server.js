ed1cbe9afa9d342bfe74244a3dc4a9b2cc694f95
Alexander Sierra <alexandercsierra@gmail.com>
2020-02-25 14:00:28 -0500

    finished lecture

--- api/server.js
+++ api/server.js
@@ -1,20 +1,55 @@
 const express = require('express');
 const helmet = require('helmet');
 const cors = require('cors');
+const session = require('express-session')
+//must come after the session
+const KnexStore = require ('connect-session-knex')(session); //remember to curry and pass the session
+
+
 
 const authRouter = require('../auth/auth-router.js');
 const usersRouter = require('../users/users-router.js');
+const knex = require('../database/dbConfig'); //needed for stroring sessions in the database
+
 
 const server = express();
+
+const sessionConfig = {
+  name: 'monster',
+  secret: 'keep it secret, keep it safe!',
+  resave: false,
+  saveUninitialized: true, //related to GDPR compliance, false in production
+  cookie: {
+    maxAge: 1000 * 60 * 10,
+    secure: false, //should be true in production
+    httpOnly: true // true means JS can't touch the cookie
+  },
+  store: new KnexStore({
+    // knex: knex
+    knex,
+    //all lowercase table where we want to store the data
+    tablename: 'sessions',
+    //if table doesn't exist, create one
+    createtable: true,
+    //column name
+    sidfieldname: 'sid',
+    //time in ms, every x min go and check and delete all sessions that have expired. Will check the expiration date, not creation date.
+    clearInterval: 1000 * 60 * 15
+  })
+}
 
 server.use(helmet());
 server.use(express.json());
 server.use(cors());
+server.use(session(sessionConfig));
 
+
+//at this point there is a req.session object created by express-session available to all endpoints
 server.use('/api/auth', authRouter);
 server.use('/api/users', usersRouter);
 
 server.get('/', (req, res) => {
+  console.log(req.session);
   res.json({ api: 'up' });
 });
 
--- auth/auth-router.js
+++ auth/auth-router.js
@@ -11,6 +11,8 @@ router.post('/register', (req, res) => {
 
   Users.add(user)
     .then(saved => {
+      //better user experience - so user doesn't have to login right after registering.
+      req.session.loggedIn = true;
       res.status(201).json(saved);
     })
     .catch(error => {
@@ -25,6 +27,8 @@ router.post('/login', (req, res) => {
     .first()
     .then(user => {
       if (user && bcrypt.compareSync(password, user.password)) {
+        req.session.loggedIn = true;
+        req.session.username = user.username;
         res.status(200).json({
           message: `Welcome ${user.username}!`,
         });
@@ -36,5 +40,21 @@ router.post('/login', (req, res) => {
       res.status(500).json(error);
     });
 });
+
+
+router.get('/logout', (req, res)=>{
+  if (req.session){
+    req.session.destroy(err => {
+      if (err){
+        res.status(500).json({message: 'you can check out any time you like, but you can never leave'})
+      } else {
+        res.status(200).json({bye: 'felicia'});        
+      }
+    });
+  } else {
+    // res.end();
+    res.status(200).json({bye: 'felicia'});
+  }
+})
 
 module.exports = router;
